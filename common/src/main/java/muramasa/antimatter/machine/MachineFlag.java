d1ce1d0c9476c7609d91c26489643f2a6ead544a
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-01-04 19:39:05 -0500

    made MachineFlag just a class with String constants instead of enums

--- common/src/main/java/muramasa/antimatter/machine/MachineFlag.java
+++ common/src/main/java/muramasa/antimatter/machine/MachineFlag.java
@@ -11,27 +11,21 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public enum MachineFlag {
-
-    BASIC, //
-    STEAM,
-    MULTI, //Has structure
-    HATCH,
-    ITEM, //Can store items
-    CELL,
-    FLUID,
-    EU, //Needs power
-    RF, //Uses RF instead of EU
-    HEAT,
-    RECIPE, //Has a recipe map
-    GUI,
-    GENERATOR, //Has a recipe map and converts applicable recipes to power.
-    COVERABLE,
-    PARTIAL_AMPS,
-    UNCULLED;
-
-    @Override
-    public String toString() {
-        return super.toString().toLowerCase();
-    }
+public final class MachineFlag {
+    public static final String BASIC = "basic"; //
+    public static final String STEAM = "steam";
+    public static final String MULTI = "multi"; //Has structure
+    public static final String HATCH = "hatch";
+    public static final String ITEM = "item"; //Can store items
+    public static final String CELL = "cell";
+    public static final String FLUID = "fluid";
+    public static final String EU = "eu"; //Needs power
+    public static final String RF = "rf"; //Uses RF instead of EU
+    public static final String HEAT = "heat";
+    public static final String RECIPE = "recipe"; //Has a recipe map
+    public static final String GUI = "gui";
+    public static final String GENERATOR = "generator"; //Has a recipe map and converts applicable recipes to power.
+    public static final String COVERABLE = "coverable";
+    public static final String PARTIAL_AMPS = "partial_amps";
+    public static final String UNCULLED = "unculled";
 }
