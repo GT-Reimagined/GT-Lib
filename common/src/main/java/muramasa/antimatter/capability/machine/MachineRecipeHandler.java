b702fa2ae8c40b5e5d321afa9e8176aa231af8a8
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-02-09 14:47:30 -0500

    removed commented out code from MachineRecipeHandler

--- common/src/main/java/muramasa/antimatter/capability/machine/MachineRecipeHandler.java
+++ common/src/main/java/muramasa/antimatter/capability/machine/MachineRecipeHandler.java
@@ -206,13 +206,6 @@ public class MachineRecipeHandler<T extends BlockEntityMachine<T>> implements IM
         if (activeRecipe.getPower() > 0 && this.tile.getPowerLevel().getVoltage() > activeRecipe.getPower()) {
             long voltage = this.activeRecipe.getPower();
             int tier = Utils.getVoltageTier(voltage);
-            /*//Dont use utils, because we allow overclocking from ulv. (If we don't just change this).
-            for (int i = 0; i < Ref.V.length; i++) {
-                if (voltage <= Ref.V[i]) {
-                    tier = i;
-                    break;
-                }
-            }*/
             long tempoverclock = (this.tile.getPowerLevel().getVoltage() / Ref.V[tier]);
             while (tempoverclock > 1) {
                 tempoverclock >>= 2;
