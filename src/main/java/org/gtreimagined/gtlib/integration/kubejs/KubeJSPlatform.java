72bef6e6767d9f3d1e4e541eee4e769177c993f3
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-04-13 22:17:19 -0400

    removed KubeJSPlatform

--- src/main/java/org/gtreimagined/gtlib/integration/kubejs/KubeJSPlatform.java
+++
@@ -1,8 +0,0 @@
-package org.gtreimagined.gtlib.integration.kubejs;
-
-import org.gtreimagined.gtlib.util.ImplLoader;
-
-public interface KubeJSPlatform {
-    KubeJSPlatform INSTANCE = ImplLoader.load(KubeJSPlatform.class);
-    void onRegister();
-}
