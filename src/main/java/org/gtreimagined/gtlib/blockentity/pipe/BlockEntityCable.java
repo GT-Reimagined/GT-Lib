6e7efa459cfb11b85f2f42ff107aee6906b45d85
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-07-10 21:44:59 -0400

    updated tesseract

--- src/main/java/org/gtreimagined/gtlib/blockentity/pipe/BlockEntityCable.java
+++ src/main/java/org/gtreimagined/gtlib/blockentity/pipe/BlockEntityCable.java
@@ -1,22 +1,13 @@
 package org.gtreimagined.gtlib.blockentity.pipe;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import org.gtreimagined.gtlib.capability.Dispatch;
-import org.gtreimagined.gtlib.gui.GuiInstance;
-import org.gtreimagined.gtlib.gui.IGuiElement;
-import org.gtreimagined.gtlib.gui.widget.InfoRenderWidget;
-import org.gtreimagined.gtlib.integration.xei.renderer.IInfoRenderer;
 import org.gtreimagined.gtlib.pipe.BlockCable;
 import org.gtreimagined.gtlib.pipe.types.Cable;
 import org.gtreimagined.gtlib.pipe.types.PipeType;
-import net.minecraft.client.gui.Font;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.util.LazyOptional;
-import org.gtreimagined.tesseract.api.capability.TesseractEUCapability;
 import org.gtreimagined.tesseract.api.eu.EUHolder;
 import org.gtreimagined.tesseract.api.eu.IEUCable;
 import org.gtreimagined.tesseract.api.forge.TesseractCaps;
@@ -26,14 +17,13 @@ import org.gtreimagined.tesseract.api.eu.IEnergyHandler;
 
 import java.util.Collection;
 
-public class BlockEntityCable<T extends PipeType<T>> extends BlockEntityPipe<T> implements IEUCable, Dispatch.Sided<IEnergyHandler>, IInfoRenderer<InfoRenderWidget.TesseractGTWidget> {
+public class BlockEntityCable<T extends PipeType<T>> extends BlockEntityPipe<T> implements IEUCable {
 
     private long holder;
     private EUNetwork network;
 
     public BlockEntityCable(T type, BlockPos pos, BlockState state) {
         super(type, pos, state);
-        pipeCapHolder.set(() -> this);
     }
 
     @Override
@@ -98,7 +88,7 @@ public class BlockEntityCable<T extends PipeType<T>> extends BlockEntityPipe<T> 
         if (!super.validate(dir)) return false;
         BlockEntity tile = this.getCachedBlockEntity(dir);
         if (tile == null) return false;
-        return tile.getCapability(TesseractCaps.ENERGY_HANDLER_CAPABILITY, dir.getOpposite()).isPresent();
+        return tile instanceof IEUCable || tile.getCapability(TesseractCaps.ENERGY_HANDLER_CAPABILITY, dir.getOpposite()).isPresent();
     }
 
     @Override
@@ -110,33 +100,6 @@ public class BlockEntityCable<T extends PipeType<T>> extends BlockEntityPipe<T> 
     protected void serverTick(Level level, BlockPos pos, BlockState state) {
         super.serverTick(level, pos, state);
         //this.setHolder(EUHolder.create(this, 0));
-    }
-
-    @Override
-    public LazyOptional<IEnergyHandler> forSide(Direction side) {
-        return LazyOptional.of(() -> new TesseractEUCapability<>(this, side, !isConnector(), (stack, dir, input, simulate) ->
-        this.coverHandler.map(t -> t.onTransfer(stack, dir, input, simulate)).orElse(false)));
-    }
-
-    @Override
-    public LazyOptional<IEnergyHandler> forNullSide() {
-        return forSide(null);
-    }
-
-    @Override
-    public void addWidgets(GuiInstance instance, IGuiElement parent) {
-        super.addWidgets(instance, parent);
-        instance.addWidget(InfoRenderWidget.TesseractGTWidget.build().setPos(10, 10));
-    }
-
-    @Override
-    public int drawInfo(InfoRenderWidget.TesseractGTWidget instance, PoseStack stack, Font renderer, int left, int top) {
-        renderer.draw(stack, "Amp average: " + instance.ampAverage, left, top, 0xFAFAFF);
-       // renderer.draw(stack, "Cable average: " + instance.cableAverage, left, top + 8, 0xFAFAFF);
-        renderer.draw(stack, "Average extracted: " + ((double) instance.voltAverage) / 20, left, top + 16, 0xFAFAFF);
-        renderer.draw(stack, "Average inserted: " + ((double) (instance.voltAverage - instance.loss)) / 20, left, top + 24, 0xFAFAFF);
-        renderer.draw(stack, "Loss average: " + (double) instance.loss / 20, left, top + 32, 0xFAFAFF);
-        return 40;
     }
 
     @Override
