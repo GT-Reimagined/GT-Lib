cc7e1329ef67e94157b5a3dab51d00ff5cdce9a1
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-07-17 19:37:32 -0400

    added method for regular mc ingredients with multiple tags

--- src/main/java/org/gtreimagined/gtlib/recipe/ingredient/RecipeIngredient.java
+++ src/main/java/org/gtreimagined/gtlib/recipe/ingredient/RecipeIngredient.java
@@ -270,6 +270,13 @@ public class RecipeIngredient extends Ingredient {
         }
         return new RecipeIngredient(new MultiValue(Arrays.stream(tagIn).map(t -> new RecipeValue(t, count))));
     }
+    @SafeVarargs
+    public static Ingredient ofIngredient(int count, TagKey<Item>... tagIn) {
+        for (TagKey<Item> itemTagKey : tagIn) {
+            ensureRegisteredTag(itemTagKey.location());
+        }
+        return new Ingredient(Arrays.stream(tagIn).map(t -> new RecipeValue(t, count)));
+    }
 
     public static RecipeIngredient ofObject(Object object, int amount){
         if (object instanceof TagKey tag){
