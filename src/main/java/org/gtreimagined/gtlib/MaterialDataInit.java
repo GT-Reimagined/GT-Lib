fd67cb34aa892ff1ecdc5485d323cad25ade0aa6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-02-08 19:47:37 -0500

    removed mortar tool

--- src/main/java/org/gtreimagined/gtlib/MaterialDataInit.java
+++ src/main/java/org/gtreimagined/gtlib/MaterialDataInit.java
@@ -48,9 +48,9 @@ public class MaterialDataInit {
                 .tool().toolDamage(WOOD.getAttackDamageBonus()).toolSpeed(WOOD.getSpeed()).toolDurability(192).toolQuality(WOOD.getLevel()).allowedToolTypes(List.of(GTTools.SOFT_HAMMER)).build();
         event.setMaterial(GTLibMaterials.Blaze).asDust().addHandleStat(-10, -0.5F, of(Enchantments.FIRE_ASPECT, 1));
 
-        event.setMaterial(GTLibMaterials.Flint).asDust(GTMaterialTypes.GEM, MaterialTags.FLINT, MaterialTags.HAS_MORTAR)
+        event.setMaterial(GTLibMaterials.Flint).asDust(GTMaterialTypes.GEM, MaterialTags.FLINT)
                 .tool().toolDamage(1.25f).toolSpeed(STONE.getSpeed()).toolDurability(128).toolQuality(1)
-                .toolEnchantments(of(Enchantments.FIRE_ASPECT, 1)).allowedToolTypes(List.of(GTTools.PICKAXE, GTTools.AXE, GTTools.SHOVEL, GTTools.SWORD, GTTools.HOE, GTTools.MORTAR, GTTools.KNIFE)).build();
+                .toolEnchantments(of(Enchantments.FIRE_ASPECT, 1)).allowedToolTypes(List.of(GTTools.PICKAXE, GTTools.AXE, GTTools.SHOVEL, GTTools.SWORD, GTTools.HOE, GTTools.KNIFE)).build();
 
         event.setMaterial(GTLibMaterials.Charcoal).asDust(GTMaterialTypes.BLOCK);
         event.setMaterial(GTLibMaterials.Coal).asGemBasic(false).asOre(0, 2, true, GTMaterialTypes.ORE_STONE);
