5291e04f72b2f7cd34865fa73a6e755406c3c7a7
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-03-10 23:11:11 -0400

    customized tooltips of gt fluid slots

--- src/main/java/org/gtreimagined/gtlib/mui/widgets/GTFluidSlot.java
+++ src/main/java/org/gtreimagined/gtlib/mui/widgets/GTFluidSlot.java
@@ -1,19 +1,59 @@
 package org.gtreimagined.gtlib.mui.widgets;
 
 import brachy.modularui.api.drawable.IDrawable;
+import brachy.modularui.api.drawable.IKey;
+import brachy.modularui.api.value.ISyncOrValue;
+import brachy.modularui.screen.RichTooltip;
+import brachy.modularui.value.sync.FluidSlotSyncHandler;
+import brachy.modularui.value.sync.SyncHandler;
 import brachy.modularui.widgets.slot.FluidSlot;
 import brachy.modularui.widgets.slot.ItemSlot;
+import com.electronwill.nightconfig.core.UnmodifiableConfig.Entry;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.fluids.FluidStack;
+import org.gtreimagined.gtlib.integration.xei.GTLibXEIPlugin;
+import org.gtreimagined.gtlib.util.FluidUtils;
+import org.gtreimagined.gtlib.util.Utils;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Accessors(fluent = true, chain = true)
 public class GTFluidSlot extends FluidSlot {
     @Setter
     IDrawable drawable;
+    private FluidSlotSyncHandler syncHandler2;
 
     @Override
     public @Nullable IDrawable getBackground() {
         return drawable;
+    }
+
+    @Override
+    protected void setSyncOrValue(@NotNull ISyncOrValue syncOrValue) {
+        super.setSyncOrValue(syncOrValue);
+        this.syncHandler2 = syncOrValue.castNullable(FluidSlotSyncHandler.class);
+    }
+
+    @Override
+    protected void addTooltip(RichTooltip tooltip) {
+        FluidStack fluid = this.syncHandler2.getValue();
+        if (fluid == null || fluid.isEmpty()) return;
+        tooltip.addLine(IKey.lang(fluid.getDisplayName()));
+        List<Component> str = new ArrayList<>();
+        int mb = fluid.getAmount();
+        str.add(Utils.translatable("gtlib.tooltip.fluid.amount", mb + " L").withStyle(ChatFormatting.BLUE));
+        str.add(Utils.translatable("gtlib.tooltip.fluid.temp", FluidUtils.getFluidTemperature(fluid.getFluid())).withStyle(ChatFormatting.RED));
+        String liquid = !FluidUtils.isFluidGaseous(fluid.getFluid()) ? "liquid" : "gas";
+        str.add(Utils.translatable("gtlib.tooltip.fluid." + liquid).withStyle(ChatFormatting.GREEN));
+        GTLibXEIPlugin.addModDescriptor(str, fluid);
+        for (Component c : str) {
+            tooltip.addLine(c);
+        }
     }
 }
