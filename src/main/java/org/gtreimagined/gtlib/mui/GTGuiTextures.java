f71a50e6cd4959416e120a83ae098bec0645c3cd
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-03-16 23:58:29 -0400

    made remaining slot and slot overlay textures use proper theme

--- src/main/java/org/gtreimagined/gtlib/mui/GTGuiTextures.java
+++ src/main/java/org/gtreimagined/gtlib/mui/GTGuiTextures.java
@@ -20,14 +20,14 @@ public class GTGuiTextures {
     public static final UITexture GT_LOGO = UITexture.fullImage(Ref.ID,"textures/gui/icons/gt_logo.png");
 
     public static final UITexture BATTERY_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/battery.png").colorType(null).build();
-    public static final UITexture CELL_SLOT_OVERLAY = UITexture.fullImage(Ref.ID, "textures/gui/slots/overlays/cell.png");
-    public static final UITexture CELL_IN_SLOT_OVERLAY = UITexture.fullImage(Ref.ID, "textures/gui/slots/overlays/cell_in.png");
-    public static final UITexture CELL_OUT_SLOT_OVERLAY = UITexture.fullImage(Ref.ID, "textures/gui/slots/overlays/cell_out.png");
-    public static final UITexture ENERGY_SLOT_OVERLAY = UITexture.fullImage(Ref.ID, "textures/gui/slots/overlays/energy.png");
+    public static final UITexture CELL_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/cell.png").defaultColorType().build();
+    public static final UITexture CELL_IN_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/cell_in.png").defaultColorType().build();
+    public static final UITexture CELL_OUT_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/cell_out.png").defaultColorType().build();
+    public static final UITexture ENERGY_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/energy.png").defaultColorType().build();
     public static final UITexture FLUID_IN_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/fluid_in.png").colorType(null).build();
     public static final UITexture FLUID_OUT_SLOT_OVERLAY = UITexture.builder().location(Ref.ID, "textures/gui/slots/overlays/fluid_out.png").colorType(null).build();
-    public static final UITexture ITEM_SLOT = UITexture.fullImage(Ref.ID, "textures/gui/slots/item.png");
-    public static final UITexture FLUID_SLOT = UITexture.fullImage(Ref.ID, "textures/gui/slots/fluid.png");
+    public static final UITexture ITEM_SLOT = UITexture.builder().location(Ref.ID, "textures/gui/slots/item.png").defaultColorType().build();
+    public static final UITexture FLUID_SLOT = UITexture.builder().location(Ref.ID, "textures/gui/slots/fluid.png").defaultColorType().build();
 
 
     public static final UITexture MACHINE_STATE = UITexture.builder().imageSize(20, 11).location(Ref.ID, "textures/gui/widgets/machine_state.png").build();
