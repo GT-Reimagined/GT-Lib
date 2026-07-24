ba584331f1682006d9f9df995d7d4c16688096a6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-07-24 16:01:25 -0400

    added fluids slots to RecipeWidget

--- src/main/java/org/gtreimagined/gtlib/integration/recipeviewer/widgets/RecipeWidget.java
+++ src/main/java/org/gtreimagined/gtlib/integration/recipeviewer/widgets/RecipeWidget.java
@@ -4,14 +4,23 @@ import brachy.modularui.drawable.UITexture;
 import brachy.modularui.drawable.progress.CompositeProgress;
 import brachy.modularui.integration.recipeviewer.RecipeSlotRole;
 import brachy.modularui.integration.recipeviewer.RecipeViewerSlotWidget;
+import brachy.modularui.integration.recipeviewer.entry.fluid.FluidStackList;
 import brachy.modularui.integration.recipeviewer.entry.item.ItemStackList;
+import brachy.modularui.screen.RichTooltip;
 import brachy.modularui.screen.viewport.ModularGuiContext;
 import brachy.modularui.theme.WidgetThemeEntry;
 import brachy.modularui.widget.ParentWidget;
 import brachy.modularui.widgets.ProgressWidget;
+import mezz.jei.api.forge.ForgeTypes;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotView;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.fluids.FluidStack;
 import org.gtreimagined.gtlib.Data;
 import org.gtreimagined.gtlib.gui.GuiProperties;
 import org.gtreimagined.gtlib.gui.SlotData;
@@ -20,12 +29,15 @@ import org.gtreimagined.gtlib.integration.recipeviewer.renderer.IRecipeInfoRende
 import org.gtreimagined.gtlib.machine.Tier;
 import org.gtreimagined.gtlib.mui.BarDir;
 import org.gtreimagined.gtlib.recipe.IRecipe;
+import org.gtreimagined.gtlib.recipe.ingredient.FluidIngredient;
 import org.gtreimagined.gtlib.recipe.ingredient.RecipeIngredient;
 import org.gtreimagined.gtlib.recipe.map.IRecipeMap;
 import org.gtreimagined.gtlib.recipe.map.RecipeMap;
+import org.gtreimagined.gtlib.util.FluidUtils;
 import org.gtreimagined.gtlib.util.Utils;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class RecipeWidget extends ParentWidget<RecipeWidget> {
@@ -124,6 +136,73 @@ public class RecipeWidget extends ParentWidget<RecipeWidget> {
                 this.child(itemStackGroup);
             }
         }
+        if (recipe.hasInputFluids()) {
+            slots = gui.getSlots().getSlots(SlotType.FL_IN, guiTier);
+            if (!slots.isEmpty()) {
+                ParentWidget<?> fluidStackGroup = new ParentWidget<>().sizeRel(1f);
+                List<FluidIngredient> fluids = recipe.getInputFluids();
+                int slotCount = Math.min(slots.size(), fluids.size());
+                for (int s = 0; s < slotCount; s++) {
+                    final int ss = s;
+                    RecipeViewerSlotWidget<?> widget = RecipeViewerSlotWidget.create()
+                            .recipeSlotRole(RecipeSlotRole.INPUT)
+                            .pos(slots.get(s).getJeiX(), slots.get(s).getJeiY())
+                            .tooltip(r -> {
+                                FluidStack stack = fluids.get(ss).getStacks()[0];
+                                createFluidTooltip(r, stack);
+                            })
+                            .value(FluidStackList.of(Arrays.asList(fluids.get(s).getStacks())));
+                    fluidStackGroup.child(widget);
+                }
+                this.child(fluidStackGroup);
+            }
+        }
+        if (recipe.hasOutputFluids()) {
+            slots = gui.getSlots().getSlots(SlotType.FL_OUT, guiTier);
+            if (!slots.isEmpty()) {
+                ParentWidget<?> fluidStackGroup = new ParentWidget<>().sizeRel(1f);
+                FluidStack[] fluids = recipe.getOutputFluids();
+                int slotCount = Math.min(slots.size(), fluids.length);
+                for (int s = 0; s < slotCount; s++) {
+                    final int ss = s;
+                    RecipeViewerSlotWidget<?> widget = RecipeViewerSlotWidget.create()
+                            .recipeSlotRole(RecipeSlotRole.OUTPUT)
+                            .pos(slots.get(s).getJeiX(), slots.get(s).getJeiY())
+                            .tooltip(r -> {
+                                FluidStack stack = fluids[ss];
+                                createFluidTooltip(r, stack);
+                            })
+                            .value(fluids[s]);
+                    fluidStackGroup.child(widget);
+                }
+                this.child(fluidStackGroup);
+            }
+        }
+    }
+
+
+    private void createFluidTooltip(RichTooltip richTooltip, FluidStack stack) {
+        Component component = null;
+/*
+        for (Component comp : list) {
+            if (comp.getContents() instanceof TranslatableContents translatable){
+                if (translatable.getKey().equals("jei.tooltip.recipe.by")){
+                    component = comp;
+                    break;
+                }
+            }
+        }
+        if (list.size() > 2) list.remove(2);
+        if (list.size() > 1) list.remove(1);
+*/
+        int mb = stack.getAmount();
+        richTooltip.addLine(Utils.translatable("gtlib.tooltip.fluid.amount", mb + " L").withStyle(ChatFormatting.BLUE));
+        richTooltip.addLine(Utils.translatable("gtlib.tooltip.fluid.temp", FluidUtils.getFluidTemperature(stack.getFluid())).withStyle(ChatFormatting.RED));
+        String liquid = !FluidUtils.isFluidGaseous(stack.getFluid()) ? "liquid" : "gas";
+        richTooltip.addLine(Utils.translatable("gtlib.tooltip.fluid." + liquid).withStyle(ChatFormatting.GREEN));
+        //if (Utils.hasNoConsumeTag(ing.getDisplayedIngredient().get().getIngredient(ForgeTypes.FLUID_STACK).get()))
+        //    list.add(Utils.literal("Does not get consumed in the process").withStyle(ChatFormatting.WHITE));
+        //if (component != null) list.add(component);
     }
 
     @Override
