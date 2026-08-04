89c18121a5a164a73e6318c5b7d7cdb24108b880
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-08-03 20:36:16 -0400

    removed old methods from IRecipeInfoRenderer

--- src/main/java/org/gtreimagined/gtlib/integration/recipeviewer/renderer/IRecipeInfoRenderer.java
+++ src/main/java/org/gtreimagined/gtlib/integration/recipeviewer/renderer/IRecipeInfoRenderer.java
@@ -10,29 +10,5 @@ import java.util.List;
 
 @FunctionalInterface
 public interface IRecipeInfoRenderer {
-    default void render(GuiGraphics stack, IRecipe recipe, Font font, int guiOffsetX, int guiOffsetY){
-
-    }
-
     List<Component> getLines(IRecipe recipe);
-
-    default int getRows(){
-        return 0;
-    }
-
-    default void renderString(GuiGraphics graphics, String string, Font font, float x, float y, int guiOffsetX, int guiOffsetY) {
-        renderString(graphics, string, font, x, y, 0xFFFFFF, guiOffsetX, guiOffsetY);
-    }
-
-    default void renderString(GuiGraphics graphics, String string, Font font, float x, float y, int color, int guiOffsetX, int guiOffsetY) {
-        renderString(graphics, string, font, x, y, color, guiOffsetX, guiOffsetY, true);
-    }
-
-    default void renderString(GuiGraphics graphics, String string, Font font, float x, float y, int color, int guiOffsetX, int guiOffsetY, boolean shadow) {
-        graphics.drawString(font, string, (guiOffsetX + x), guiOffsetY + y, color, shadow);
-    }
-
-    default int stringWidth(String string, Font font) {
-        return font.width(string);
-    }
 }
