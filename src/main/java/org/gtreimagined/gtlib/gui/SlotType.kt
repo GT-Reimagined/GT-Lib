c287b9c4e98a2182ce0a7db44b8966e5cd1b4f9e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-08-26 21:26:07 -0400

    rewrote SlotType and SlotTypes in kotlin

--- src/main/java/org/gtreimagined/gtlib/gui/SlotType.kt
+++ src/main/java/org/gtreimagined/gtlib/gui/SlotType.kt
@@ -1,83 +1,96 @@
-package org.gtreimagined.gtlib.gui;
+package org.gtreimagined.gtlib.gui
 
-import brachy.modularui.drawable.UITexture;
-import brachy.modularui.widgets.slot.ModularSlot;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
-import org.gtreimagined.gtlib.GTAPI;
-import org.gtreimagined.gtlib.capability.IGuiHandler;
-import org.gtreimagined.gtlib.capability.fluid.FluidTanks;
-import org.gtreimagined.gtlib.machine.event.IMachineEvent;
-import org.gtreimagined.gtlib.mui.GTGuiTextures;
-import org.gtreimagined.gtlib.registration.IGTObject;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.items.IItemHandler;
+import brachy.modularui.drawable.UITexture
+import brachy.modularui.widgets.slot.ModularSlot
+import net.minecraft.world.item.ItemStack
+import net.minecraftforge.items.IItemHandler
+import org.gtreimagined.gtlib.GTAPI
+import org.gtreimagined.gtlib.capability.IGuiHandler
+import org.gtreimagined.gtlib.capability.fluid.FluidTanks
+import org.gtreimagined.gtlib.machine.event.IMachineEvent
+import org.gtreimagined.gtlib.mui.GTGuiTextures
+import org.gtreimagined.gtlib.registration.IGTObject
+import java.util.function.BiPredicate
+import java.util.function.Consumer
+import java.util.function.Function
 
-import java.util.Map;
-import java.util.function.BiPredicate;
-import java.util.function.Consumer;
-import java.util.function.Function;
-
-public record SlotType<T extends ModularSlot>(
-        String id, ISlotSupplier<T> slotSupplier, Function<IGuiHandler, FluidTanks> fluidHandlerSupplier,
-        boolean mayPickup, boolean mayPlace, boolean allowExternalOutput, boolean allowExternalInput,
-        boolean phantom, boolean slotGroup, UITexture background, UITexture overlay, BiPredicate<IGuiHandler, ItemStack> tester
-) implements IGTObject, IMachineEvent {
-    public String getId() {
-        return id;
+@JvmRecord
+data class SlotType<T : ModularSlot>(
+    val id: String,
+    @JvmField val slotSupplier: ISlotSupplier<T>?,
+    @JvmField val fluidHandlerSupplier: Function<IGuiHandler, FluidTanks>?,
+    @JvmField val mayPickup: Boolean, @JvmField val mayPlace: Boolean, @JvmField val allowExternalOutput: Boolean,
+    @JvmField val allowExternalInput: Boolean, @JvmField val phantom: Boolean, @JvmField val slotGroup: Boolean,
+    @JvmField val background: UITexture?, @JvmField val overlay: UITexture?,
+    @JvmField val tester: BiPredicate<IGuiHandler, ItemStack>?
+) : IGTObject, IMachineEvent {
+    init {
+        require(!(slotSupplier == null && fluidHandlerSupplier == null)) { "Slot Type must have either a fluid handler supplier or item slot supplier!" }
     }
 
-    public interface ISlotSupplier<T extends ModularSlot> {
-        T get(SlotType<T> type, IGuiHandler tile, Map<SlotType<?>, IItemHandler> slots, int index, SlotData<T> data);
+    override fun getId(): String {
+        return id
     }
 
-    public static <T extends ModularSlot> SlotType<T> create(Consumer<SlotTypeBuilder<T>> consumer){
-        SlotTypeBuilder<T> b = new SlotTypeBuilder<T>();
-        consumer.accept(b);
-        if (b.id == null) {
-            throw new IllegalArgumentException("Missing id for slot type");
+    fun interface ISlotSupplier<T : ModularSlot> {
+        fun get(type: SlotType<T>, tile: IGuiHandler, slots: Map<SlotType<*>, IItemHandler>, index: Int, data: SlotData<T>): T
+    }
+
+    companion object {
+        @JvmStatic fun <T : ModularSlot> create(consumer: Consumer<SlotTypeBuilder<T>>): SlotType<T>  = SlotTypeBuilder.create(consumer)
+    }
+
+    class SlotTypeBuilder<T : ModularSlot> {
+        var id: String? = null
+        var slotSupplier: ISlotSupplier<T>? = null
+        var fluidHandlerSupplier: Function<IGuiHandler, FluidTanks>? = null
+        var mayPickup = true
+        var mayPlace = true
+        var allowExternalOutput = true
+        var allowExternalInput = true
+        var phantom = false
+        var slotGroup = true
+        var background: UITexture? = GTGuiTextures.ITEM_SLOT
+        var overlay: UITexture? = null
+        var tester = BiPredicate { g: IGuiHandler, i: ItemStack -> true }
+
+        fun id(id: String): SlotTypeBuilder<T>  = apply { this.id = id }
+        fun slotSupplier(supplier: ISlotSupplier<T>?): SlotTypeBuilder<T> = apply {this.slotSupplier = supplier}
+        fun fluidHandlerSupplier(function: Function<IGuiHandler, FluidTanks>) = apply { this.fluidHandlerSupplier = function }
+        fun mayPickup(mayPickup: Boolean) = apply { this.mayPickup = mayPickup }
+        fun mayPlace(mayPlace: Boolean) = apply { this.mayPlace = mayPlace }
+        fun allowExternalInput(allowExternalInput: Boolean) = apply { this.allowExternalInput = allowExternalInput }
+        fun allowExternalOutput(allowExternalOutput: Boolean) = apply { this.allowExternalOutput = allowExternalOutput }
+        fun phantom(phantom: Boolean) = apply { this.phantom = phantom }
+        fun slotGroup(slotGroup: Boolean) = apply { this.slotGroup = slotGroup }
+        fun background(background: UITexture) = apply { this.background = background }
+        fun overlay(overlay: UITexture) = apply { this.overlay = overlay }
+
+        private fun build(id: String): SlotType<T> {
+            return SlotType(
+                id,
+                slotSupplier,
+                fluidHandlerSupplier,
+                mayPickup,
+                mayPlace,
+                allowExternalOutput,
+                allowExternalInput,
+                phantom,
+                slotGroup,
+                background,
+                overlay,
+                tester
+            )
         }
-        if (b.slotSupplier == null && b.fluidHandlerSupplier == null) {
-            throw new IllegalArgumentException("Slot Type must have either a fluid handler supplier or item slot supplier!");
-        }
-        SlotType<T> slotType = b.build();
-        GTAPI.register(SlotType.class, slotType);
-        return slotType;
-    }
-
-    @Accessors(chain = true, fluent = false)
-    @Getter
-    @Setter
-    public static class SlotTypeBuilder<T extends ModularSlot> {
-        private String id;
-        private ISlotSupplier<T> slotSupplier;
-        private Function<IGuiHandler, FluidTanks> fluidHandlerSupplier;
-        private boolean mayPickup = true;
-        private boolean mayPlace = true;
-        private boolean allowExternalOutput = true;
-        private boolean allowExternalInput = true;
-        private boolean phantom = false;
-        private boolean slotGroup = true;
-        private UITexture texture = GTGuiTextures.ITEM_SLOT;
-        private UITexture overlay;
-        private BiPredicate<IGuiHandler, ItemStack> tester = (g, i) -> true;
-
-        private SlotType<T> build(){
-            return new SlotType<>(
-                    id,
-                    slotSupplier,
-                    fluidHandlerSupplier,
-                    mayPickup,
-                    mayPlace,
-                    allowExternalOutput,
-                    allowExternalInput,
-                    phantom,
-                    slotGroup,
-                    texture,
-                    overlay,
-                    tester
-            );
+        companion object {
+            fun <T : ModularSlot> create(consumer: Consumer<SlotTypeBuilder<T>>): SlotType<T> {
+                val b = SlotTypeBuilder<T>()
+                consumer.accept(b)
+                val id = requireNotNull(b.id) { "Missing id for slot type" }
+                val slotType = b.build(id)
+                GTAPI.register(SlotType::class.java, slotType)
+                return slotType
+            }
         }
     }
 }
--- src/main/java/org/gtreimagined/gtlib/gui/SlotTypes.kt
+++ src/main/java/org/gtreimagined/gtlib/gui/SlotTypes.kt
@@ -1,156 +1,245 @@
-package org.gtreimagined.gtlib.gui;
+package org.gtreimagined.gtlib.gui
 
-import brachy.modularui.widgets.slot.ModularSlot;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.items.wrapper.EmptyHandler;
-import org.gtreimagined.gtlib.blockentity.BlockEntityMachine;
-import org.gtreimagined.gtlib.capability.FluidHandler;
-import org.gtreimagined.gtlib.capability.FluidHandler.FluidTankType;
-import org.gtreimagined.gtlib.capability.IGuiHandler;
-import org.gtreimagined.gtlib.capability.fluid.FluidTanks;
-import org.gtreimagined.gtlib.cover.ICover;
-import org.gtreimagined.gtlib.gui.slot.AbstractSlot;
-import org.gtreimagined.gtlib.gui.slot.SlotCell;
-import org.gtreimagined.gtlib.gui.slot.SlotEnergy;
-import org.gtreimagined.gtlib.gui.slot.SlotFake;
-import org.gtreimagined.gtlib.gui.slot.SlotInput;
-import org.gtreimagined.gtlib.gui.slot.SlotOutput;
-import org.gtreimagined.gtlib.mui.GTGuiTextures;
-import org.gtreimagined.tesseract.api.forge.TesseractCaps;
+import brachy.modularui.widgets.slot.ModularSlot
+import net.minecraft.world.item.ItemStack
+import net.minecraftforge.common.capabilities.ForgeCapabilities
+import net.minecraftforge.items.wrapper.EmptyHandler
+import org.gtreimagined.gtlib.blockentity.BlockEntityMachine
+import org.gtreimagined.gtlib.capability.FluidHandler
+import org.gtreimagined.gtlib.capability.IGuiHandler
+import org.gtreimagined.gtlib.capability.fluid.FluidTanks
+import org.gtreimagined.gtlib.cover.ICover
+import org.gtreimagined.gtlib.gui.slot.AbstractSlot
+import org.gtreimagined.gtlib.gui.slot.SlotCell
+import org.gtreimagined.gtlib.gui.slot.SlotEnergy
+import org.gtreimagined.gtlib.gui.slot.SlotFake
+import org.gtreimagined.gtlib.gui.slot.SlotInput
+import org.gtreimagined.gtlib.gui.slot.SlotOutput
+import org.gtreimagined.gtlib.mui.GTGuiTextures
+import org.gtreimagined.tesseract.api.forge.TesseractCaps
+import java.util.function.BiPredicate
+import java.util.function.Function
 
-import java.util.function.BiPredicate;
+object SlotTypes {
 
-public class SlotTypes {
-    static final BiPredicate<IGuiHandler, ItemStack> NO_INPUT = (gu, stack) -> false;
-    static final BiPredicate<IGuiHandler, ItemStack> ITEM_IN_PRED = (g, stack) -> {
-        if (g instanceof BlockEntityMachine<?> machine) {
-            return machine.recipeHandler.map(rh -> rh.accepts(stack)).orElse(true);
+    private val NO_INPUT: BiPredicate<IGuiHandler, ItemStack> = BiPredicate { _, _ -> false }
+
+    private val ITEM_IN_PRED: BiPredicate<IGuiHandler, ItemStack> = BiPredicate { g, stack ->
+        if (g is BlockEntityMachine<*>) {
+            return@BiPredicate g.recipeHandler.map {
+                it.accepts(stack)
+            }.orElse(false)!!
         }
-        return true;
-    };
-    public static SlotType<SlotInput> IT_IN = SlotType.create(b -> {
-        b.setId("item_in");
-        b.setSlotSupplier((type, gui, inv, i, d) -> new SlotInput(type, gui, inv.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setTester(ITEM_IN_PRED);
-        b.setAllowExternalOutput(false);
-    });
-    public static SlotType<SlotOutput> IT_OUT = SlotType.create(b -> {
-        b.setId("item_out");
-        b.setSlotSupplier((type, gui, inv, i, d) -> new SlotOutput(type, gui, inv.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setTester(NO_INPUT);
-        b.setSlotGroup(false);
-        b.setAllowExternalInput(false);
-        b.setMayPlace(false);
-    });
-    public static SlotType<SlotFake> DISPLAY = SlotType.create(b -> {
-        b.setId("display");
-        b.setSlotSupplier((type, gui, item, i, d) -> new SlotFake(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i, false));
-        b.setTester(NO_INPUT);
-        b.setSlotGroup(false);
-        b.setAllowExternalInput(false);
-        b.setAllowExternalOutput(false);
-        b.setMayPlace(false);
-        b.setMayPickup(false);
-        b.setPhantom(true);
-    });
-    public static SlotType<SlotFake> DISPLAY_SETTABLE = SlotType.create(b -> {
-        b.setId("display_settable");
-        b.setSlotSupplier((type, gui, item, i, d) -> new SlotFake(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i, true));
-        b.setTester(NO_INPUT);
-        b.setSlotGroup(false);
-        b.setAllowExternalInput(false);
-        b.setAllowExternalOutput(false);
-        b.setPhantom(true);
-    });
-    public static SlotType<AbstractSlot<?>> STORAGE = SlotType.create(b -> {
-        b.setId("storage");
-        b.setSlotSupplier((type, gui, item, i, d) -> new AbstractSlot<>(type, gui, item.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setTester((t, i) -> true);
-    });
-    public static SlotType<SlotCell> CELL_IN = SlotType.create(b -> {
-        b.setId("cell_in");
-        b.setSlotSupplier((type, gui, inv, i, d) -> new SlotCell(type, gui, inv.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setTester((t, i) -> i.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).isPresent());
-        b.setAllowExternalOutput(false);
-        b.setOverlay(GTGuiTextures.CELL_IN_SLOT_OVERLAY);
-    });
-    public static SlotType<SlotCell> CELL_OUT = SlotType.create(b -> {
-        b.setId("cell_out");
-        b.setSlotSupplier((type, gui, inv, i, d) -> new SlotCell(type, gui, inv.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setTester((t, i) -> i.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).isPresent());
-        b.setAllowExternalInput(false);
-        b.setMayPlace(false);
-        b.setSlotGroup(false);
-        b.setOverlay(GTGuiTextures.CELL_OUT_SLOT_OVERLAY);
-    });
-    public static SlotType<SlotEnergy> ENERGY = SlotType.create(b -> {
-        b.setId("energy");
-        b.setSlotSupplier((type, gui, inv, i, d) -> new SlotEnergy(type, gui, inv.getOrDefault(type, EmptyHandler.INSTANCE), i));
-        b.setTester((t, i) -> {
-                    if (t instanceof BlockEntityMachine<?> machine) {
-                        return machine.energyHandler.map(eh -> {
-                            return i.getCapability(TesseractCaps.ENERGY_HANDLER_CAPABILITY_ITEM).map(inner -> {
-                                return ((inner.getInputVoltage() | inner.getOutputVoltage()) == (eh.getInputVoltage() | eh.getOutputVoltage()));
-                            }).orElse(i.getCapability(ForgeCapabilities.ENERGY).isPresent());
-                        }).orElse(false);
-                    }
-                    return true;
-                });
-        b.setAllowExternalOutput(false);
-        b.setOverlay(GTGuiTextures.ENERGY_SLOT_OVERLAY);
-    });
-    public static SlotType<ModularSlot> FL_IN = SlotType.create(b -> {
-        b.setId("fluid_in");
-        b.setFluidHandlerSupplier(g -> {
-            if (g instanceof BlockEntityMachine<?> machine) {
-                return machine.fluidHandler.map(FluidHandler::getInputTanks).orElse(FluidTanks.EMPTY_TANK);
+        true
+    }
+
+    @JvmField
+    var IT_IN: SlotType<SlotInput> = SlotType.create { b ->
+        b.id = "item_in"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, inv, i, d ->
+            SlotInput(
+                type,
+                gui,
+                inv.getOrDefault(type, EmptyHandler.INSTANCE),
+                i
+            )
+        }
+        b.tester = ITEM_IN_PRED
+        b.allowExternalOutput = false
+    }
+
+    @JvmField
+    val IT_OUT: SlotType<SlotOutput> = SlotType.create { b ->
+        b.id = "item_out"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, inv, i, d ->
+            SlotOutput(
+                type,
+                gui,
+                inv.getOrDefault(type, EmptyHandler.INSTANCE),
+                i
+            )
+        }
+        b.tester = NO_INPUT
+        b.slotGroup = false
+        b.allowExternalInput = false
+        b.mayPlace = false
+    }
+
+    @JvmField
+    val DISPLAY: SlotType<SlotFake> = SlotType.create { b ->
+        b.id = "display"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, d ->
+            SlotFake(
+                type,
+                gui,
+                item.getOrDefault(type, EmptyHandler.INSTANCE),
+                i,
+                false
+            )
+        }
+        b.tester = NO_INPUT
+        b.slotGroup = false
+        b.allowExternalInput = false
+        b.allowExternalOutput = false
+        b.mayPlace = false
+        b.mayPickup = false
+        b.phantom = true
+    }
+
+    @JvmField
+    val DISPLAY_SETTABLE: SlotType<SlotFake> = SlotType.create { b ->
+        b.id = "display_settable"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, d ->
+            SlotFake(
+                type,
+                gui,
+                item.getOrDefault(type, EmptyHandler.INSTANCE),
+                i,
+                true
+            )
+        }
+        b.tester = NO_INPUT
+        b.slotGroup = false
+        b.allowExternalInput = false
+        b.allowExternalOutput = false
+        b.phantom = true
+    }
+
+    @JvmField
+    val STORAGE: SlotType<AbstractSlot<*>> = SlotType.create { b ->
+        b.id = "storage"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, item, i, d ->
+            AbstractSlot(
+                type,
+                gui,
+                item.getOrDefault(type, EmptyHandler.INSTANCE),
+                i
+            )
+        }
+        b.tester = BiPredicate { _, _ -> true }
+    }
+
+    @JvmField
+    val CELL_IN: SlotType<SlotCell> = SlotType.create { b ->
+        b.id = "cell_in"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, inv, i, d ->
+            SlotCell(
+                type,
+                gui,
+                inv.getOrDefault(type, EmptyHandler.INSTANCE),
+                i
+            )
+        }
+        b.tester = BiPredicate { _, i -> i.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).isPresent }
+        b.allowExternalOutput = false
+        b.overlay = GTGuiTextures.CELL_IN_SLOT_OVERLAY
+    }
+
+    @JvmField
+    val CELL_OUT: SlotType<SlotCell> = SlotType.create { b ->
+        b.id = "cell_out"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, inv, i, d ->
+            SlotCell(
+                type,
+                gui,
+                inv.getOrDefault(type, EmptyHandler.INSTANCE),
+                i
+            )
+        }
+        b.tester = BiPredicate { _, i -> i.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).isPresent }
+        b.allowExternalInput = false
+        b.mayPlace = false
+        b.slotGroup = false
+        b.overlay = GTGuiTextures.CELL_OUT_SLOT_OVERLAY
+    }
+
+    @JvmField
+    val ENERGY: SlotType<SlotEnergy> = SlotType.create { b ->
+        b.id = "energy"
+        b.slotSupplier = SlotType.ISlotSupplier { type, gui, inv, i, d ->
+            SlotEnergy(
+                type,
+                gui,
+                inv.getOrDefault(type, EmptyHandler.INSTANCE),
+                i
+            )
+        }
+        b.tester = BiPredicate { t, i ->
+            if (t is BlockEntityMachine<*>) {
+                return@BiPredicate t.energyHandler.map { eh ->
+                    i.getCapability(TesseractCaps.ENERGY_HANDLER_CAPABILITY_ITEM).map { inner ->
+                        (inner.inputVoltage or inner.outputVoltage) == (eh.inputVoltage or eh.outputVoltage)
+                    }.orElse(i.getCapability(ForgeCapabilities.ENERGY).isPresent)
+                }.orElse(false) == true
             }
-            if (g instanceof ICover cover) {
-                if (cover.getFluidTanks() != null) {
-                    return cover.getFluidTanks().getOrDefault(FluidTankType.INPUT, FluidTanks.EMPTY_TANK);
+            true
+        }
+        b.allowExternalOutput = false
+        b.overlay = GTGuiTextures.ENERGY_SLOT_OVERLAY
+    }
+
+    @JvmField
+    val FL_IN: SlotType<ModularSlot> = SlotType.create { b ->
+        b.id = "fluid_in"
+        b.fluidHandlerSupplier = Function { g ->
+            if (g is BlockEntityMachine<*>) {
+                return@Function g.fluidHandler.map { it.inputTanks }
+                    .orElse(FluidTanks.EMPTY_TANK)!!
+            }
+            if (g is ICover) {
+                if (g.fluidTanks != null) {
+                    return@Function g.fluidTanks
+                        .getOrDefault(FluidHandler.FluidTankType.INPUT, FluidTanks.EMPTY_TANK)
                 }
             }
-            return FluidTanks.EMPTY_TANK;
-        });
-        b.setTexture(GTGuiTextures.FLUID_SLOT);
-        b.setOverlay(GTGuiTextures.FLUID_IN_SLOT_OVERLAY);
-    });
+            FluidTanks.EMPTY_TANK
+        }
+        b.background = GTGuiTextures.FLUID_SLOT
+        b.overlay = GTGuiTextures.FLUID_IN_SLOT_OVERLAY
+    }
+
     //Cheat using same ID to get working counter.
-    public static SlotType<ModularSlot> FL_OUT = SlotType.create(b -> {
-        b.setId("fluid_out");
-        b.setFluidHandlerSupplier(g -> {
-            if (g instanceof BlockEntityMachine<?> machine) {
-                return machine.fluidHandler.map(FluidHandler::getOutputTanks).orElse(FluidTanks.EMPTY_TANK);
+    @JvmField
+    val FL_OUT: SlotType<ModularSlot> = SlotType.create { b ->
+        b.id = "fluid_out"
+        b.fluidHandlerSupplier = Function { g ->
+            if (g is BlockEntityMachine<*>) {
+                return@Function g.fluidHandler.map<FluidTanks?> { it.outputTanks }
+                    .orElse(FluidTanks.EMPTY_TANK)!!
             }
-            if (g instanceof ICover cover) {
-                if (cover.getFluidTanks() != null) {
-                    return cover.getFluidTanks().getOrDefault(FluidTankType.OUTPUT, FluidTanks.EMPTY_TANK);
+            if (g is ICover) {
+                if (g.fluidTanks != null) {
+                    return@Function g.fluidTanks
+                        .getOrDefault(FluidHandler.FluidTankType.OUTPUT, FluidTanks.EMPTY_TANK)
                 }
             }
-            return FluidTanks.EMPTY_TANK;
-        });
-        b.setTexture(GTGuiTextures.FLUID_SLOT);
-        b.setOverlay(GTGuiTextures.FLUID_OUT_SLOT_OVERLAY);
-    });
-    public static SlotType<ModularSlot> FL_PHANTOM = SlotType.create(b -> {
-        b.setId("fluid_phantom");
-        b.setFluidHandlerSupplier(g -> {
-            if (g instanceof BlockEntityMachine<?> machine) {
-                return machine.fluidHandler.map(FluidHandler::getPhantomTanks).orElse(FluidTanks.EMPTY_TANK);
+            FluidTanks.EMPTY_TANK
+        }
+        b.background = GTGuiTextures.FLUID_SLOT
+        b.overlay = GTGuiTextures.FLUID_OUT_SLOT_OVERLAY
+    }
+
+    @JvmField
+    val FL_PHANTOM: SlotType<ModularSlot> = SlotType.create { b ->
+        b.id = "fluid_phantom"
+        b.fluidHandlerSupplier = Function { g ->
+            if (g is BlockEntityMachine<*>) {
+                return@Function g.fluidHandler.map { it.phantomTanks }
+                    .orElse(FluidTanks.EMPTY_TANK)!!
             }
-            if (g instanceof ICover cover) {
-                if (cover.getFluidTanks() != null) {
-                    return cover.getFluidTanks().getOrDefault(FluidTankType.PHANTOM, FluidTanks.EMPTY_TANK);
+            if (g is ICover) {
+                if (g.fluidTanks != null) {
+                    return@Function g.fluidTanks
+                        .getOrDefault(FluidHandler.FluidTankType.PHANTOM, FluidTanks.EMPTY_TANK)
                 }
             }
-            return FluidTanks.EMPTY_TANK;
-        });
-        b.setTexture(GTGuiTextures.FLUID_SLOT);
-        b.setPhantom(true);
-    });
+            FluidTanks.EMPTY_TANK
+        }
+        b.background = GTGuiTextures.FLUID_SLOT
+        b.phantom = true
+    }
 
-    public static void init(){
-
+    @JvmStatic
+    fun init() {
     }
 }
