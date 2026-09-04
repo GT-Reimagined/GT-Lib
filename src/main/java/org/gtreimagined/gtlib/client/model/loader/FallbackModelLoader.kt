df9a81f26bcdca97fd4acc1184c556fbede8b086
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-09-04 15:18:28 -0400

    made base and fallback models in Fallback models able to either be strings or json objects

--- src/main/java/org/gtreimagined/gtlib/client/model/loader/FallbackModelLoader.kt
+++ src/main/java/org/gtreimagined/gtlib/client/model/loader/FallbackModelLoader.kt
@@ -1,7 +1,9 @@
 package org.gtreimagined.gtlib.client.model.loader
 
 import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import com.ibm.icu.impl.LocaleUtility.fallback
 import net.minecraft.client.renderer.block.model.BlockModel
 import net.minecraft.client.resources.model.UnbakedModel
 import net.minecraft.resources.ResourceLocation
@@ -9,10 +11,14 @@ import org.gtreimagined.gtlib.client.model.FallbackModel
 
 class FallbackModelLoader(resourceLocation: ResourceLocation) : GTModelLoader<FallbackModel>(resourceLocation) {
     override fun read(jsonObject: JsonObject, context: JsonDeserializationContext): FallbackModel {
-        val base = JsonObject()
-        base.addProperty("parent", jsonObject["base"].asString)
-        val fallback = JsonObject()
-        fallback.addProperty("parent", jsonObject["fallback"].asString)
+        val baseElement: JsonElement = jsonObject["base"]
+        val base = if (baseElement is JsonObject) baseElement else JsonObject().also {
+            it.addProperty("parent", baseElement.asString)
+        }
+        val fallbackElement: JsonElement = jsonObject["fallback"]
+        val fallback = if (fallbackElement is JsonObject) fallbackElement else JsonObject().also {
+            it.addProperty("parent", fallbackElement.asString)
+        }
         val baseModel: UnbakedModel  = context.deserialize<BlockModel>(base, BlockModel::class.java)
         val fallbackModel: UnbakedModel  = context.deserialize<BlockModel>(fallback, BlockModel::class.java)
         return FallbackModel(baseModel, fallbackModel)
