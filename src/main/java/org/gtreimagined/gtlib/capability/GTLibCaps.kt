f89cc3b069a20c52b1e6f079a252fec84106a07a
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-09-25 19:03:36 -0400

    fixed compile error

--- src/main/java/org/gtreimagined/gtlib/capability/GTLibCaps.kt
+++ src/main/java/org/gtreimagined/gtlib/capability/GTLibCaps.kt
@@ -5,4 +5,5 @@ import com.google.common.collect.BiMap
 import com.google.common.collect.HashBiMap
 import net.minecraftforge.common.capabilities.Capability
 
+@JvmField
 val CAP_MAP: BiMap<Class<*>, Capability<*>> = HashBiMap.create()
